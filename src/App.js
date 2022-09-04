d041dc3a46912d2fab53eba8b911ba520db81d57
Ajay Ranjan <ajayranjan@live.com>
2022-09-04 14:03:28 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-blue-600 font-bold text-2xl">Testing GH Pages</h2>
+    </div>
+  );
+}
+
+export default App;
